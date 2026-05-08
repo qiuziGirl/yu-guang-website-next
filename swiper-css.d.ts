608c4d5beefaf208fd196e3250920e4671d5e22e
qiuzi <2515632823@qq.com>
2026-05-08 20:20:52 +0800

    chore(deps): 升级 TypeScript 到 6.0.3 并添加 Swiper CSS 类型声明
    - 升级 TypeScript 从 5.9.2 到 6.0.3
    - 更新所有相关依赖的 TypeScript 版本引用
    - 新增 swiper-css.d.ts 文件，为 Swiper CSS 模块添加类型声明
    - 修改 tsconfig.json 中 jsx 配置从 "preserve" 改为 "react-jsx"
    - 更新 pnpm-lock.yaml 中所有 TypeScript 相关依赖的版本

---
+++ swiper-css.d.ts
@@ -0,0 +1,5 @@
+// Swiper CSS imports have no type declarations
+declare module "swiper/css";
+declare module "swiper/css/effect-fade";
+declare module "swiper/css/navigation";
+declare module "swiper/css/pagination";
